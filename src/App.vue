9e92fa59d8d05f8a994260dbc14c5a92bb68dda4
Yasser AZAIEZ <yaz@atolcd.com>
2022-09-25 16:10:30 +0200

    Init projet vite

---
+++ src/App.vue
@@ -0,0 +1,119 @@
+<script>
+import CatsTab from './components/CatsTab.vue';
+export default {
+  components: {
+    CatsTab
+  },
+  data() {
+    return {
+    cats : [
+      {
+        "Nom": "Pitchoune",
+        "Sexe": "Femelle",
+        "Né le": "2019-10-01",
+        "Race": "Gouttière",
+        "Prix": "1100,60",
+        "Commentaires": "Aime le calme du foyer, sort peu.  Ne va jamais chasser. N'aime pas les croquettes mais ne mange que des patées. Attention à ne pas laisser près des enfants."
+      },
+      {
+        "Nom": "Yamcha",
+        "Sexe": "Mâle",
+        "Né le": "2016-01-26",
+        "Race": "Balinois",
+        "Prix": "800,00",
+        "Commentaires": "Jolie robe couleur sable,miaule beaucoup"
+      },
+      {
+        "Nom": "Poukie",
+        "Sexe": "Femelle",
+        "Né le": "2015-09-16",
+        "Race": "Chartreux",
+        "Prix": "150,60",
+        "Commentaires": "N'aime pas ses congénères"
+      },
+      {
+        "Nom": "Plume",
+        "Sexe": "Femelle",
+        "Né le": "2019-10-01",
+        "Race": "Maine Coon",
+        "Prix": "1500,00",
+        "Commentaires": "Espiègle et joueuse"
+      },
+      {
+        "Nom": "Zizou",
+        "Sexe": "Mâle",
+        "Né le": "2019-01-30",
+        "Race": "Européen",
+        "Prix": "10,00",
+        "Commentaires": "SOS animal malade"
+      },
+      {
+        "Nom": "Séréna",
+        "Sexe": "Femelle",
+        "Né le": "2017-04-01",
+        "Race": "Ecaille de tortue",
+        "Prix": "100,00",
+        "Commentaires": "chatte timide et réservée"
+      },
+      {
+        "Nom": "Pitchin",
+        "Sexe": "Femelle",
+        "Né le": "2019-10-01",
+        "Race": "Gouttière",
+        "Prix": "1100,60",
+        "Commentaires": "Aime le calme du foyer, sort peu"
+      },
+      {
+        "Nom": "Yamchou",
+        "Sexe": "Mâle",
+        "Né le": "2016-01-26",
+        "Race": "Balinois",
+        "Prix": "800,00",
+        "Commentaires": "Jolie robe couleur sable,miaule beaucoup"
+      },
+      {
+        "Nom": "Poukette",
+        "Sexe": "Femelle",
+        "Né le": "2015-09-16",
+        "Race": "Chartreux",
+        "Prix": "100,60",
+        "Commentaires": "N'aime pas ses congénères"
+      },
+      {
+        "Nom": "Plumo",
+        "Sexe": "Mâle",
+        "Né le": "2018-10-01",
+        "Race": "Maine Coon",
+        "Prix": "1500,25",
+        "Commentaires": "Espiègle et joueuse"
+      },
+      {
+        "Nom": "Zilou",
+        "Sexe": "Mâle",
+        "Né le": "2019-11-30",
+        "Race": "Européen",
+        "Prix": "10,00",
+        "Commentaires": "SOS animal malade"
+      },
+      {
+        "Nom": "Vaiana",
+        "Sexe": "Femelle",
+        "Né le": "2017-04-01",
+        "Race": "Ecaille de tortue",
+        "Prix": "100,00",
+        "Commentaires": "chatte timide et réservée"
+      }
+    ]
+    
+  }
+}
+}
+
+</script>
+
+<template>
+  <CatsTab :cats="cats"></CatsTab>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/CatsTab.vue
@@ -0,0 +1,52 @@
+<script>
+
+  export default {
+    name: 'CatsTab',
+    props:{
+      cats: {
+        type: Array,
+        required: true      }
+    },
+
+    methods: {
+      handleEdit(index , row){
+        console.log(index, row)
+      },
+      handleDelete(index, row){
+        console.log(index, row)
+      }
+    }
+  }
+
+
+</script>
+
+<template>
+        <li>{{ cats.length }}</li>
+
+  <el-table :data="cats" style="width: 100%">
+      <el-table-column label="Nom" prop="Nom" />
+      <el-table-column label="Sexe" prop="Sexe" />
+      <el-table-column label="Né le " prop="Né le" />
+      <el-table-column label="Prix" prop="Prix" />
+      <el-table-column label="Commentaire" prop="Commentaire" />
+      <li>{{ cats }}</li>
+
+      <el-table-column align="right">
+        <template #default="scope">
+          <el-button size="small" @click="handleEdit(scope.$index, scope.row)"
+            >Edit</el-button
+          >
+          <el-button
+            size="small"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)"
+            >Delete</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+</template>
+
+<style scoped>
+</style>
